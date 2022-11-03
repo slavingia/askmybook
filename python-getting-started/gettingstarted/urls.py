0b61e3a141d8462d88fe8ca85977f838bb61d85d
Sahil Lavingia <sahil.lavingia@gmail.com>
2022-11-03 14:26:50 -0700

    cp

---
+++ python-getting-started/gettingstarted/urls.py
@@ -0,0 +1,21 @@
+from django.urls import path, include
+
+from django.contrib import admin
+
+admin.autodiscover()
+
+import hello.views
+
+# To add a new path, first import the app:
+# import blog
+#
+# Then add the new path:
+# path('blog/', blog.urls, name="blog")
+#
+# Learn more here: https://docs.djangoproject.com/en/2.1/topics/http/urls/
+
+urlpatterns = [
+    path("", hello.views.index, name="index"),
+    path("db/", hello.views.db, name="db"),
+    path("admin/", admin.site.urls),
+]
