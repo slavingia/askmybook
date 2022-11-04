a2e284a0aa63d4c1600a5421d8daaa8826829a38
Sahil Lavingia <sahil.lavingia@gmail.com>
2022-11-04 16:43:33 -0700

    cp

--- hello/views.py
+++ hello/views.py
@@ -136,7 +136,7 @@ def ask(request):
     if previous_answer:
         previous_question.ask_count = previous_question.ask_count + 1
         previous_question.save()
-        return render(request, "answer.html", { "answer": previous_answer, "question": question })
+        return render(request, "answer.html", { "answer": previous_answer, "question": question, "audio_src_url": previous_question.audio_src_url })
 
     df = pd.read_csv("https://askbook.s3.us-east-1.amazonaws.com/pages.csv?response-content-disposition=inline&X-Amz-Security-Token=IQoJb3JpZ2luX2VjEPT%2F%2F%2F%2F%2F%2F%2F%2F%2F%2FwEaCXVzLXdlc3QtMiJHMEUCIQCosaeIJrUiCuzmLKO2PgXleWPusM85rA0iDsUqZmUNWQIgPwJAqgNu%2BtJyEfD0m0zvIWpgBWyrwtNH449DpPbHJDEq7QII3f%2F%2F%2F%2F%2F%2F%2F%2F%2F%2FARACGgw1OTkzNTI3OTA5NzgiDMyp840t9%2FPinfvxQSrBAh0gXgLija9B8WABaoVjfJlR%2B11eQiks2cxjW0YVeJ4KJYqaS8gkO0tZ%2FcoSnsoQ%2FdmlLuv6z7qHAN3L5s1Yd%2BP4m65JQ%2B0icnQnh8SEE3K0cx2JDlQ9Uf3EVciwujswpYtVu8JbRQAE40rYcHlhkHbA9r5FGw13NbL7Y9Z9dpuBcnLDSgbxo%2BQQSKE9dkLe8C70T9oF1A5ejREpZ2dG4xUZTUouiNFB%2BW586C23hWMsn0kP1z5esWxFFuGIzxY8HwVvpj%2Fe8IBU%2BkO1cabXjNAygx8iKLU1nLQcor1QmZYw1F7CgqLrm22oQT%2FGf%2FGGvp5AL2SGAryQxHeyf2Hz3%2BOABGJYUd5DE7FO4PyyGBHJw1kLcZUflNOCbUIVyGF2dWi6sATZJcxz06i%2FUb6z06z2fEWeNQiNMOHOFxCQ%2FvSvXjD93pWbBjqzAsnHm1Wz7Gk5L6FR4v%2FyrgVDiKaow6b7G5hmLd4xba%2FBuiwfuntO1oSew99oUy0i0aigsjFoAN%2FEvIcXzvFiP%2BqHnBsVjiOB%2BkAP%2F1oxd3fTJvZ6ezzp3DHwb%2BHnod%2FwA93A0Bh6%2FT4WBDFNkBNRxfFb%2F9M4xba477vg9mg%2Fb0%2F7excYq7RY8AcZ%2F%2FAxBeMHZjhwpRyKZC4FBB3JYTjoGEbDSF7NhijX%2FZKmNQcN0HQ7n6%2FOs1%2F0%2F3nRUCtUTJUgL%2FQypxsDL55%2FW9jM5Wv0f7nyopaGDQZ2mKMZwyeUSpL%2B%2BVP3wsapWU6jQnfvWvCa%2F4qEd5flSt11yj8O90iuGweKz22f6AfJhOMF6aFh1Pu97e85AbzVsPVO%2FdzaXqz2mnY9gxb4CPezeHGScqaS3kkWE30%3D&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Date=20221104T200413Z&X-Amz-SignedHeaders=host&X-Amz-Expires=43200&X-Amz-Credential=ASIAYXDBVZ7BOCJHYKXV%2F20221104%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Signature=cbabc981e72dab93354b87021a8d84cd49df9bd4cb43980ea789ef666262f22f")
     df = df.set_index(["title"])
@@ -153,7 +153,7 @@ def ask(request):
     voice_uuid = '0eb3a3f1'
     callback_uri = 'https://askbook.herokuapp.com/callback/resemble-clip'
 
-    Resemble.v2.clips.create_async(
+    Resemble.v2.clips.create_sync(
         project_uuid,
         voice_uuid,
         callback_uri,
@@ -168,6 +168,12 @@ def ask(request):
     )
 
     return render(request, "answer.html", { "answer": answer, "question": question.question })
+
+def callback_resemble_clip(request):
+    question = Question.objects.filter(id=request.POST.get("id", ""))
+    question.audio_src_url = request.POST.get("audio_src_url", "")
+    question.save()
+    return HttpResponse('')
 
 def db(request):
     questions = Question.objects.all().order_by('-ask_count')
