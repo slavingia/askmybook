bc73edf0996a1aeadecde0936cbcecd53fab8b59
Sahil Lavingia <sahil.lavingia@gmail.com>
2022-11-04 15:44:16 -0700

    ask count

--- hello/models.py
+++ hello/models.py
@@ -4,6 +4,4 @@ class Question(models.Model):
     question = models.CharField(max_length=140)
     answer = models.CharField(max_length=280)
     created_at = models.DateTimeField("date created", auto_now_add=True)
-
-class Greeting(models.Model):
-    when = models.DateTimeField("date created", auto_now_add=True)
+    ask_count = models.IntegerField(default=0)
