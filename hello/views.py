ec5fc894ca411de5fe7685ebfc51fbed80f0a5e4
Sahil Lavingia <sahil.lavingia@gmail.com>
2022-11-12 14:46:02 -0800

    cp

--- hello/views.py
+++ hello/views.py
@@ -253,8 +253,8 @@ def metadata(request):
     data = ""
 
     for question in Question.objects.all().order_by('-ask_count'):
-        if not question.real_answer:
-            continue
+        # if not question.real_answer:
+        #     continue
         data = data + "{ \"prompt\": \"" + question.question + "\", \"completion\": \" " + question.answer if not question.real_answer else question.real_answer + "\" }\n"
 
     response = HttpResponse(data, content_type="application/jsonl")
